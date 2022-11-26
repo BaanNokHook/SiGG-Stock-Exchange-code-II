23433c161d8d796f054d1a4b2ad80f22f4625657
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-27 06:53:45 +0700

    SiGG-Stock-Exchange-code-II-State-III

---
+++ BankMicroservice/Controllers/BankTransactionController.cs
@@ -0,0 +1,37 @@
+﻿using BankMicroservice.Entities;
+using BankMicroservice.Persistances.ReturnTypes;
+using BankMicroservice.Services.BankTransactions;
+using ErrorHandlingDll.ReturnTypes;
+using Microsoft.AspNetCore.Mvc;
+using Sentry.Extensibility;
+
+namespace BankMicroservice.Controllers
+{
+    [Controller]  
+
+    public class BankTransactionService : Controller
+    {
+        private readonly IBankTransactionService bankTransactionService;
+
+        public BankTransactionController(IBankTransactionService bankTransactionService)
+        {
+            _bankTransactionService = bankTransactionService;   
+        }
+
+        /// <summary>
+        /// Gets a bank transaction by id
+        /// </summary>
+        /// <param name="transactionId"></param>
+        /// <returns></returns>   
+        [HttpGet("Api/BankTransactions/{transactionId}")]    
+        public async Task<IActionResult> GetTransaction([FromRoute] long transactionId)
+        {
+            ReturnModel<BankTransactionModel> result = _bankTransactionService.GetTransaction(transactionId).Result;
+            return StatusCode((int)result.HttpStatusCode, result);  
+        }
+
+    }
+
+}
+
+
---
+++ BankMicroservice/Dtos/Bank/Sadad/SadadPaymentDataDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BankMicroservice.Dtos.Bank.Sadad
+{
+    public class SadaPaymentDataDto
+    {
+        public string MerchantId { get; set;  }  
+        public string TermianlId { get; set;  }  
+        public int Amount { get; set; }  
+        public string OrderId { get; set; }  
+        public DateTime LocalDateTime { get; set; }  
+        public string SignData { get; set; }  
+        public string ReturnUrl { get; set; }
+
+        // public string PurchasePage { get; set; }  
+        public SadadPaymentDataDto()
+        {
+
+        }
+        public SadadPaymentDataDto(string key, int amount, string returnUrl, string orderId, string merchantId, string terminalId)
+        {
+            Amount = amount;
+            OrderId = orderId;
+            MerchantId = merchantId;
+            SignData = key;
+            ReturnUrl = returnUrl;
+            TerminalId = terminalId;
+            LocalDateTime = DateTime.Now;
+            // PurchasePage = purchasePage;
+        }
+    }
+}
